f1a2074ad369357d2db4c939085a01afb78932bf
Delilah Necrason <delilahnecrason@gmail.com>
2022-01-12 17:23:42 -0500

    Add seed data

--- db/db/seeds/dev/images.js
+++ db/db/seeds/dev/images.js
@@ -1,5 +1,3 @@
-const imageData = require('../imageData')
-
 exports.seed = function(knex) {
   // Deletes ALL existing entries
   return knex('images').del()
@@ -7,7 +5,7 @@ exports.seed = function(knex) {
       // Inserts seed entries
       return knex('table_name').insert([
         {
-          id: "1",
+          id: '1',
           url: 'https://images.unsplash.com/photo-1549887552-cb1071d3e5ca?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=930&q=80',
           title: 'Prometheus Bound and the Oceanids',
           color: ['white', 'black', 'grey'],
@@ -18,7 +16,7 @@ exports.seed = function(knex) {
           price: '25'
         },
         {
-          id: "2",
+          id: '2',
           url: 'https://images.unsplash.com/photo-1555443805-658637491dd4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1585&q=80',
           title: 'Dea',
           color: ['blue', 'white'],
@@ -29,7 +27,7 @@ exports.seed = function(knex) {
           price: '25'
         },
         {
-          id: "3",
+          id: '3',
           url: 'https://images.unsplash.com/photo-1506813561347-cbbdf7b3f520?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1002&q=80',
           title: 'Ercole e Ippolita',
           color: ['black', 'white', 'yellow'],
@@ -40,7 +38,7 @@ exports.seed = function(knex) {
           price: '25'
         },
         {
-          id: "4",
+          id: '4',
           url: 'https://images.unsplash.com/photo-1628508438706-6e6a19853e1a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80',
           title: 'Homer Head',
           color: ['purple'],
@@ -51,7 +49,7 @@ exports.seed = function(knex) {
           price: '25'
         },
         {
-          id: "5",
+          id: '5',
           url: 'https://images.unsplash.com/photo-1614701759345-c4e5dfa3a0b9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80',
           title: 'Posiedon',
           color: ['Blue'],
@@ -62,7 +60,7 @@ exports.seed = function(knex) {
           price: '25'
         },
         {
-          id: "6",
+          id: '6',
           url: 'https://images.unsplash.com/photo-1484589065579-248aad0d8b13?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NDN8fHBob3RvZ3JhcGh5JTIwYXJ0fGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60',
           title: 'Spacey Milky Way',
           color: ['yellow', 'black', 'orange'],
@@ -73,7 +71,7 @@ exports.seed = function(knex) {
           price: '25'
         },
         {
-          id: "7",
+          id: '7',
           url: 'https://images.unsplash.com/photo-1549490349-8643362247b5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80',
           title: 'Textured Purple',
           color: ['purple'],
@@ -84,7 +82,7 @@ exports.seed = function(knex) {
           price: '25'
         },
         {
-          id: "8",
+          id: '8',
           url: 'https://images.unsplash.com/photo-1553356084-58ef4a67b2a7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80',
           title: 'Bubble Gum',
           color: ['pink', 'blue'],
@@ -95,7 +93,7 @@ exports.seed = function(knex) {
           price: '25'
         },
         {
-          id: "9",
+          id: '9',
           url: 'https://images.unsplash.com/photo-1607457661772-02cb7eb0511b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80',
           title: 'Ovaltine',
           color: ['brown', 'black'],
@@ -106,7 +104,7 @@ exports.seed = function(knex) {
           price: '25'
         },
         {
-          id: "10",
+          id: '10',
           url: 'https://images.unsplash.com/photo-1550537687-c91072c4792d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80',
           title: 'Shades',
           color: ['yellow', 'white', 'grey'],
@@ -117,7 +115,7 @@ exports.seed = function(knex) {
           price: '25'
         },
         {
-          id: "11",
+          id: '11',
           url: 'https://images.unsplash.com/photo-1500462918059-b1a0cb512f1d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80',
           title: 'Hallway',
           color: ['pink', 'blue', 'orange'],
@@ -128,7 +126,7 @@ exports.seed = function(knex) {
           price: '25'
         },
         {
-          id: "12",
+          id: '12',
           url: 'https://images.unsplash.com/photo-1543691110-1c7d6b1927f6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80',
           title: 'SOHOHO',
           color: ['orange', 'red'],
@@ -139,7 +137,7 @@ exports.seed = function(knex) {
           price: '25'
         },
         {
-          id: "13",
+          id: '13',
           url: 'https://images.unsplash.com/photo-1590905655658-1e3c2f511c6c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=992&q=80',
           title: 'Build Bridges Not Walls',
           color: ['orange', 'brown'],
@@ -150,7 +148,7 @@ exports.seed = function(knex) {
           price: '25'
         },
         {
-          id: "14",
+          id: '14',
           url: 'https://images.unsplash.com/photo-1630406644170-1b974365b8fd?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1828&q=80',
           title: 'STOP SEXUALIZING GIRLS',
           color: ['black', 'grey', 'white'],
@@ -161,7 +159,7 @@ exports.seed = function(knex) {
           price: '25'
         },
         {
-          id: "15",
+          id: '15',
           url: 'https://images.unsplash.com/photo-1583824187027-e27ae4bbb091?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=988&q=80',
           title: 'Team Spirit',
           color: ['black', 'grey', 'white'],
@@ -172,7 +170,7 @@ exports.seed = function(knex) {
           price: '25'
         },
         {
-          id: "16",
+          id: '16',
           url: 'https://images.unsplash.com/photo-1577081320692-6eff449819c0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=896&q=80',
           title: 'Last Summer Things Were Greener',
           color: ['green', 'blue', 'brown', 'black'],
@@ -183,7 +181,7 @@ exports.seed = function(knex) {
           price: '25'
         },
         {
-          id: "17",
+          id: '17',
           url: 'https://images.unsplash.com/photo-1579783900882-c0d3dad7b119?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=816&q=80',
           title: 'Vase of Flowers',
           color: ['multi', 'black'],
@@ -194,7 +192,7 @@ exports.seed = function(knex) {
           price: '25'
         },
         {
-          id: "18",
+          id: '18',
           url: 'https://images.unsplash.com/photo-1580136608260-4eb11f4b24fe?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1452&q=80',
           title: 'Springtime',
           color: ['green', 'blue', 'white'],
@@ -205,7 +203,7 @@ exports.seed = function(knex) {
           price: '25'
         },
         {
-          id: "19",
+          id: '19',
           url: 'https://images.unsplash.com/photo-1576769267415-9642010aa962?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1372&q=80',
           title: 'Changing the Letter',
           color: ['red', 'blue', 'green', 'yellow'],
@@ -216,7 +214,7 @@ exports.seed = function(knex) {
           price: '25'
         },
         {
-          id: "20",
+          id: '20',
           url: 'https://images.unsplash.com/photo-1577720580479-7d839d829c73?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1568&q=80',
           title: 'Near Brodick, Isle Of Arran, Scotland',
           color: ['yellow', 'blue', 'green'],
@@ -227,7 +225,7 @@ exports.seed = function(knex) {
           price: '25'
         },
         {
-          id: "21",
+          id: '21',
           url: 'https://images.unsplash.com/photo-1634986666676-ec8fd927c23d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1635&q=80',
           title: 'Digital David',
           color: ['purple', 'blue'],
@@ -238,7 +236,7 @@ exports.seed = function(knex) {
           price: '25'
         },
         {
-          id: "22",
+          id: '22',
           url: 'https://images.unsplash.com/photo-1637140945341-f28ada987326?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1035&q=80',
           title: 'Hand Rising',
           color: ['black', 'white', 'silver'],
@@ -249,7 +247,7 @@ exports.seed = function(knex) {
           price: '25'
         }, 
         {
-          id: "23",
+          id: '23',
           url: 'https://images.unsplash.com/photo-1542772144-515ddfae17e9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1035&q=80',
           title: 'Alley Colorful',
           color: ['orange', 'blue'],
@@ -260,7 +258,7 @@ exports.seed = function(knex) {
           price: '25'
         }, 
         {
-          id: "24",
+          id: '24',
           url: 'https://images.unsplash.com/photo-1637717256696-a0204d03a8fe?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1064&q=80',
           title: 'Watermelon Astronaut',
           color: ['red', 'green', 'white'],
@@ -271,7 +269,7 @@ exports.seed = function(knex) {
           price: '25'
         }, 
         {
-          id: "25",
+          id: '25',
           url: 'https://images.unsplash.com/photo-1629729802306-2c196af7eef5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1180&q=80',
           title: 'Membrane',
           color: ['purple', 'orange'],
@@ -282,7 +280,7 @@ exports.seed = function(knex) {
           price: '25'
         }, 
         {
-          id: "26",
+          id: '26',
           url: 'https://images.unsplash.com/photo-1632516643720-e7f5d7d6ecc9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1611&q=80',
           title: 'Perfect Circle',
           color: ['orange', 'blue'],
@@ -293,7 +291,7 @@ exports.seed = function(knex) {
           price: '25'
         }, 
         {
-          id: "27",
+          id: '27',
           url: 'https://images.unsplash.com/flagged/photo-1573803625411-9edf9a6ae3b9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1035&q=80',
           title: 'Graffiti Doodle',
           color: ['yellow', 'purple', 'white', 'black'],
@@ -304,7 +302,7 @@ exports.seed = function(knex) {
           price: '25'
         },
         {
-          id: "28",
+          id: '28',
           url: 'https://images.unsplash.com/photo-1616427593347-c0d7817e7f88?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80',
           title: 'Self Portrait',
           color: ['black', 'white', 'yellow'],
@@ -315,7 +313,7 @@ exports.seed = function(knex) {
           price: '25'
         },
         {
-          id: "29",
+          id: '29',
           url: 'https://images.unsplash.com/photo-1582140161538-22f8c4ab7345?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=970&q=80',
           title: 'unknown',
           color: ['black', 'white', 'yellow'],
@@ -326,7 +324,7 @@ exports.seed = function(knex) {
           price: '25'
         },
         {
-          id: "30",
+          id: '30',
           url: 'https://images.unsplash.com/photo-1577086677645-1e5e43894316?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1599&q=80',
           title: 'Anxiety',
           color: ['black', 'white', 'grey'],
@@ -337,7 +335,7 @@ exports.seed = function(knex) {
           price: '25'
         },
         {
-          id: "31",
+          id: '31',
           url: 'https://images.unsplash.com/photo-1526336024174-e58f5cdd8e13?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80',
           title: 'Kitty Cat with Friendo',
           color: ['orange', 'white', 'grey'],
@@ -348,7 +346,7 @@ exports.seed = function(knex) {
           price: '25'
         },
         {
-          id: "32",
+          id: '32',
           url: 'https://images.unsplash.com/photo-1548802673-380ab8ebc7b7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1035&q=80',
           title: 'Sleepy Cat',
           color: ['orange', 'white'],
@@ -359,7 +357,7 @@ exports.seed = function(knex) {
           price: '25'
         },
         {
-          id: "33",
+          id: '33',
           url: 'https://images.unsplash.com/photo-1516280030429-27679b3dc9cf?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80',
           title: 'Thinking Cat',
           color: ['black', 'green'],
@@ -370,7 +368,7 @@ exports.seed = function(knex) {
           price: '25'
         },
         {
-          id: "34",
+          id: '34',
           url: 'https://images.unsplash.com/photo-1603725480633-45d1f1021fa4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80',
           title: 'Drop',
           color: ['red'],
@@ -381,7 +379,7 @@ exports.seed = function(knex) {
           price: '25'
         },
         {
-          id: "35",
+          id: '35',
           url: 'https://images.unsplash.com/photo-1616247380767-5ebadc9b869e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1742&q=80',
           title: 'Blue Girl',
           color: ['blue', 'white'],
@@ -392,7 +390,7 @@ exports.seed = function(knex) {
           price: '25'
         },
         {
-          id: "36",
+          id: '36',
           url: 'https://images.unsplash.com/photo-1603344204980-4edb0ea63148?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1035&q=80',
           title: 'Doodles',
           color: ['black', 'white'],
@@ -405,19 +403,3 @@ exports.seed = function(knex) {
       ]);
     });
 };
-
-// const createImages = async ()
-
-
-// exports.seed = async function (knex) {
-//   try {
-//     await knex('images').del() // delete all images first
-
-//     // Insert a single image, return id
-//     const imageId = await knex('papers').insert({
-//       title: 'Fooo', author: 'Bob', publisher: 'Minnesota'
-//     }, 'id')
-//   } catch (error) {
-//     console.log(`Error seeding data: ${error}`);
-//   }
-// }
